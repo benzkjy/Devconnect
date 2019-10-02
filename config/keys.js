37e54ad9922df5b82fbe2006664483dfabe472f3
Khajornyot <benzpos@gmail.com>
2019-10-02 19:43:14 +0700

    added authentication system, i.e. register, login, validation.

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURL:
-    "mongodb://benzpos:benzbenz1@ds139567.mlab.com:39567/projectmernstack"
+    "mongodb://benzpos:benzbenz1@ds139567.mlab.com:39567/projectmernstack",
+  secretOrKey: "secret"
 };
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const users = require("./routes/api/users");
 const posts = require("./routes/api/posts");
@@ -7,6 +9,11 @@ const profile = require("./routes/api/profile");
 
 const app = express();
 
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// DB config
 const db = require("./config/keys").mongoURL;
 
 mongoose
@@ -14,7 +21,11 @@ mongoose
   .then(() => console.log(`MongoDB Connected`))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hi I very like u so much"));
+//Passport middleware
+app.use(passport.initialize());
+
+//Passport Config
+require("./config/passport")(passport);
 
 //use routes
 app.use("/api/users", users);
