159454e6c29cfb78c87f70f170a1dcaaac79491d
Khajornyot <benzpos@gmail.com>
2019-10-12 19:17:54 +0700

    finished all routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -61,7 +61,7 @@ router.post("/register", (req, res) => {
   });
 });
 
-// @route   GET api/users/login
+// @route   POST api/users/login
 // @desc    Login users /Return JWT token
 // @access  Public
 router.post("/login", (req, res) => {
